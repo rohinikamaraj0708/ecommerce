7077794a018f7122319ce2157c972b0441e6bbc0
rohinikamaraj0708 <rohinikamaraj0708@gmail.com>
2026-07-10 19:24:59 +0530

    add home route

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,10 +1,16 @@
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-
 from django.urls import path, include
 
+
+def home(request):
+    return HttpResponse("Ecommerce Backend API is running 🚀")
+
+
 urlpatterns = [
+    path("", home),   # 👈 இந்த line add பண்ண வேண்டும்
     path("admin/", admin.site.urls),
     path("api/", include("store.urls")),
 ]
